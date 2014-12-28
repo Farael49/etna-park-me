5af02679a03fd5cae85dce5ea271287e55c0ed45
christophe.douy49@gmail.com <christophe.douy49@gmail.com@0a8582cf-30fe-e9dd-7264-cb51a2bebf71>
2014-12-28 16:32:06 +0000

    First commit, project structure and some code

---
+++ Code/android_project/ParkMe/src/classes/User.java
@@ -0,0 +1,55 @@
+package classes;
+
+import java.util.List;
+
+/**
+ * Created by Christophe on 16/12/2014.
+ */
+public class User {
+	private String email; // Used as an ID, not PID
+	private static User instance;
+	public static boolean isAuthenticated;
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Vehicle> getVehicles() {
+		return vehicles;
+	}
+
+	public void setVehicles(List<Vehicle> vehicles) {
+		this.vehicles = vehicles;
+	}
+
+	public String getAuthToken() {
+		return authToken;
+	}
+
+	public void setAuthToken(String authToken) {
+		this.authToken = authToken;
+	}
+
+	private List<Vehicle> vehicles; // List containing User's vehicles
+	private String authToken;
+
+	public User(){
+		
+	}
+	public User(String email, String authToken) {
+		this.email = email;
+		this.authToken = authToken;
+	}
+	
+	public static User getInstance(){
+		if(instance==null){
+			instance = new User();
+			return instance;
+		}else{
+			return instance;
+		}
+	}
+}
